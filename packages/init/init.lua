25a8d50f18a290130e05bb25da891313188dcf80
TsarN <nikts53@gmail.com>
2015-05-31 17:07:59 +0300

    Init now uses /etc/inittab style

--- packages/init/init.lua
+++ packages/init/init.lua
@@ -4,44 +4,78 @@ thread.registerSignal("INT", function() end)
 thread.registerSignal("TERM", function() end)
 thread.registerSignal("KILL", function() end)
 
-function usage()
+local function usage()
     print("Usage: init [runlevel]")
 end
 
-function run(level)
+local function run(level)
     kernel.log("New runlevel: " .. level)
-    local f = fs.open("/etc/rc.d/rc" .. level, "r")
+    if not fs.exists("/etc/inittab") then
+        kernel.panic("/etc/inittab not found")
+        return
+    end
+    local f = fs.open("/etc/inittab", "r")
     local lst = string.split(f.readAll(), "\n")
-    f.close()
-    table.sort(lst, function(a, b)
-        return tonumber(string.sub(a, 2, 3)) < tonumber(string.sub(b, 2, 3))
-    end)
+    local inittab = {}
     for k, v in pairs(lst) do
-        if string.sub(v, 1, 1) == "R" then
-            os.run({}, "/etc/init.d/" .. string.sub(v, 4, #v), "start")
-        end
-        if string.sub(v, 1, 1) == "S" then
-            os.run({}, "/etc/init.d/" .. string.sub(v, 4, #v), "stop")
-        end
-        if string.sub(v, 1, 1) == "K" then
-            os.run({}, "/etc/init.d/" .. string.sub(v, 4, #v), "restart")
+        local tmp = string.split(v, ":")
+        if tmp[3] ~= "off" then
+            inittab[tmp[1]] = {
+                runlevels = tmp[2],
+                process = tmp[4],
+                action = tmp[3]
+            }
         end
     end
-    if level == 0 then if acpi then acpi.shutdown() else os.shutdown() end end
-    if level == 6 then if acpi then acpi.reboot() else os.reboot() end end
-    while true do sleep(999999) end
+    for k, v in pairs(inittab) do
+        if v.runlevels:match(tostring(level)) then
+            local pause = false
+            if v.action == "wait" then pause = true end
+            kernel.log("[init] Starting " .. v.process)
+            local pid = thread.runFile(v.process, nil, pause)
+            if not pause then
+                if v.action == "respawn" then
+                    kernel.registerHook("dead", function(dpid)
+                        if dpid == pid then
+                            kernel.log("[init] Restarting " .. v.process)
+                            pid = thread.runFile(v.process)
+                        end
+                    end)
+                end
+            end
+        end
+    end
+    kernel.registerHook("dead", function(pid)
+        local t = thread.getRunningThreads()
+        local r = 0
+        for k, v in pairs(t) do
+            if not v.dead then
+                r = r + 1
+            end
+        end
+        if r <= 1 then
+            print("System halted")
+            while true do
+                coroutine.yield()
+            end
+            return
+        end
+    end)
+    while true do
+        coroutine.yield()
+    end
 end
 
 local argv = { ... }
 if #argv == 0 then
-    run(1)
+    run(3)
     kernel.panic("Init done. Running init 0")
     run(0)
     return
-    elseif argv[1] == "--help" then
-        usage()
-        kernel.log("Stopping init")
-        return
-    else
-        run(math.floor(tonumber(argv[1])))
-    end
+elseif argv[1] == "--help" then
+    usage()
+    kernel.log("Stopping init")
+    return
+else
+    run(math.floor(tonumber(argv[1])))
+end
